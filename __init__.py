ac6599df7281de6b411f47edc19eae3210596af9
e <chenjiayao@aobi.com>
2022-12-01 14:49:01 +0800

    1. add wxwork_robot; 2. add conf_logging_by_yml; 3. add benchmark

--- __init__.py
+++ __init__.py
@@ -1,2 +1,4 @@
 from gautils.mysqldb import MysqlDb, MysqlQuery, connect_mysql
 from gautils.conf import Conf
+from gautils.qwx import WXWorkRobot
+from gautils.utils import benchmark, conf_logging_by_yml
