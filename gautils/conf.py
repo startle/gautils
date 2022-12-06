3c559b7f310a257f83fe2f5ccdcdbf2d97de03a9
chenjiayao <chenjiayao@aobi.com>
2022-12-06 18:49:18 +0800

    get_dict

--- gautils/conf.py
+++ gautils/conf.py
@@ -8,18 +8,33 @@ class Conf:
         with open(conf_path, 'r') as f:
             yml = yaml.safe_load(f.read())
             self.datas = yml
-    def get(self, path, default=None):
+    def get(self, path, default=None, ignore_none = False):
+        d = self._get()
+        if isinstance(d, str): d = d.strip()
+        return d
+    def _get(self, path, default=None, ignore_none = False):
         d = self.datas
         for k in path:
             if k not in d:
-                if default is None: raise ValueError('conf not found.[%s]' % ('.'.join(path)))
+                if not ignore_none: raise ValueError('conf path not found. [%s]' % self._path_to_str(path))
                 else: return default
             d = d[k]
-        if isinstance(d, str): d = d.strip()
         return d
-    def get_int(self, path, default=None): return int(self.get(path, default=default))
-    def get_float(self, path, default=None): return float(self.get(path, default=default))
-    def get_bool(self, path, default=None): return bool(self.get(path, default=default))
+    def get_int(self, path, default=None, ignore_none = False): return int(self.get(path, default=default,ignore_none=ignore_none))
+    def get_float(self, path, default=None, ignore_none = False): return float(self.get(path, default=default,ignore_none=ignore_none))
+    def get_bool(self, path, default=None, ignore_none = False): return bool(self.get(path, default=default,ignore_none=ignore_none))
+    def get_dict(self, path, default=None, ignore_none = False): 
+        data = self._get(path)
+        if isinstance(data, dict): return data
+        else: raise ValueError('path not dict. [%s]', self._path_to_str(path))
+    def _path_to_str(self, path): return ('.'.join(path))
 
 if __name__ == '__main__':
+    from collections import namedtuple
+    DbConf = namedtuple('DbConf', ['host','port','account','pwd','db'])
+
+    conf = Conf('conf.yml')
+    db = conf.get_dict(['db'])
+    db = DbConf(**db)
+    print(db)
     pass
