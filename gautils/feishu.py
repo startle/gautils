866eeb6e81d4d2fd0d3026342f325af74d62eddf
e <chenjiayao@aobi.com>
2024-11-25 10:55:28 +0800

    nothing.

--- gautils/feishu.py
+++ gautils/feishu.py
@@ -1,10 +1,11 @@
 import requests
-    
-def send_fs_robot_msg(url, msgs=None, json=None):
+
+def send_fs_robot_msg(url, msgs: str | list[str] = None, json=None):
     if msgs is not None and isinstance(msgs, str):
         msgs = [msgs]
     if json is None:
-        if msgs is None: raise Exception('empty msgs&json')
+        if msgs is None:
+            raise Exception('empty msgs&json')
         msg_content = '\n'.join(msgs)
         json = {
             "msg_type": "text",
