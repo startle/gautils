75000240d6a9f03c9fae1248ba45afb1452360de
e <chenjiayao@aobi.com>
2025-03-16 13:14:09 +0800

    bug fixed.

--- gautils/coroutine.py
+++ gautils/coroutine.py
@@ -6,37 +6,38 @@ from typing import Callable, List, Dict
 class CScheduler:
     def __init__(self):
         self.lines: Dict[str, Dict] = {}
-    def register_line(self, line: str, interval_s: float, start_time: datetime, end_time: datetime):
+    def register_line(self, line: str, interval_s: float, is_loop=True, start_time: datetime = None, end_time: datetime = None):
         self.lines[line] = {
             'interval': interval_s,
             'tasks': [],
             'start_time': start_time,
-            'end_time': end_time
+            'end_time': end_time,
+            'is_loop': is_loop,
         }
-    def register_tasks(self, line: str, tasks: List[Callable], is_loop=True):
+    def register_tasks(self, line: str, tasks: List[Callable]):
         if line in self.lines:
-            self.lines[line]['tasks'].append({
-                'tasks': list(tasks),
-                'is_loop': is_loop
-            })
+            for task in tasks:
+                self.lines[line]['tasks'].append(task)
     def is_active(self, line: str) -> bool:
         current_time = datetime.now()
         line_data = self.lines[line]
         return line_data['start_time'] <= current_time < line_data['end_time']
     async def _run_line(self, line: str):
         data = self.lines[line]
-        await asyncio.sleep(max(0, (data['start_time'] - datetime.now()).total_seconds()))
 
-        while self.is_active(line):
-            for task_set in data['tasks']:
-                for task in task_set['tasks']:
-                    try:
-                        task()
-                    except Exception as e:
-                        logging.error(f"task error", exc_info=e)
-                if not task_set['is_loop']:
-                    task_set['tasks'].clear()
+        async def run_tasks():
+            for task in data['tasks']:
+                try:
+                    task()
+                except Exception as e:
+                    logging.error(f"task error", exc_info=e)
                 await asyncio.sleep(data['interval'])
+        if data['is_loop']:
+            await asyncio.sleep(max(0, (data['start_time'] - datetime.now()).total_seconds()))
+            while self.is_active(line):
+                await run_tasks()
+        else:
+            await run_tasks()
     async def _run(self):
         tasks = [self._run_line(line) for line in self.lines]
         await asyncio.gather(*tasks)
